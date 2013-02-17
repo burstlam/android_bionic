c32377967e76fd02b7eb5f68cb0430700d1f9416
Steve Kondik <shade@chemlab.org>
2013-02-16 18:48:17 -0800

    libc: Add optimized memcpy for Cortex-A15
     * This version is from Newlib and is written/recommended by ARM as the best
       routine for Cortex-A15.
    
    Change-Id: Ie9c2817dabda929ae4efaada5f6d467d53551ba4
    
    Conflicts:
    	libc/Android.mk

--- libc/Android.mk
+++ libc/Android.mk
@@ -377,7 +377,6 @@ libc_common_src_files += \
 	arch-arm/bionic/tgkill.S \
 	arch-arm/bionic/memcmp.S \
 	arch-arm/bionic/memcmp16.S \
-	arch-arm/bionic/memcpy.S \
 	arch-arm/bionic/memset.S \
 	arch-arm/bionic/setjmp.S \
 	arch-arm/bionic/sigsetjmp.S \
@@ -385,6 +384,13 @@ libc_common_src_files += \
 	arch-arm/bionic/syscall.S \
 	string/strncmp.c \
 	unistd/socketcalls.c
+
+# We have a special memcpy for A15 currently
+ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a15)
+libc_common_src_files += arch-arm/bionic/memcpy-a15.S
+else
+libc_common_src_files += arch-arm/bionic/memcpy.S
+endif
 
 # Check if we want a neonized version of memmove instead of the
 # current ARM version
